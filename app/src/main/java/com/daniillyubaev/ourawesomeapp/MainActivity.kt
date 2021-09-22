023c4e27c88c661c9a7ddd7a301ee769c237d1f4
Daniil <ddtarski@gmail.com>
2021-09-22 15:13:15 +0300

    Add loading users from API, make saving users

--- app/src/main/java/com/daniillyubaev/ourawesomeapp/MainActivity.kt
+++ app/src/main/java/com/daniillyubaev/ourawesomeapp/MainActivity.kt
@@ -2,34 +2,56 @@ package com.daniillyubaev.ourawesomeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.daniillyubaev.ourawesomeapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    val viewModel: MainViewModel by viewModels()
+    companion object {
+        val LOG_TAG = "MyAwesomeLogTag"
+    }
+
+    private val viewModel: MainViewModel by viewModels()
     private val viewBinding by viewBinding(ActivityMainBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val adapter = setupRecyclerView()
-
-        findViewById<View>(R.id.usersRecyclerView).isVisible = false
-        findViewById<View>(R.id.progressBar).isVisible = true
+        Log.d(LOG_TAG, "onCreate()")
+        setContentView(R.layout.activity_main)
+        setupRecyclerView()
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                adapter.userList = loadUsers()
-                adapter.notifyDataSetChanged()
-                findViewById<View>(R.id.usersRecyclerView).isVisible = true
-                findViewById<View>(R.id.progressBar).isVisible = false
+                viewModel.viewState.collect { viewState ->
+                    Log.d(LOG_TAG, "$viewState")
+                   renderViewState(viewState)
+                }
+            }
+        }
+    }
+
+    private fun renderViewState(viewState: MainViewModel.ViewState) {
+        when (viewState) {
+            is MainViewModel.ViewState.Loading -> {
+                viewBinding.usersRecyclerView.isVisible = false
+                viewBinding.progressBar.isVisible = true
+            }
+            is MainViewModel.ViewState.Data -> {
+                viewBinding.usersRecyclerView.isVisible = true
+                (viewBinding.usersRecyclerView.adapter as UserAdapter).apply{
+                    userList = viewState.userList
+                    notifyDataSetChanged()
+                }
+                viewBinding.progressBar.isVisible = false
             }
         }
     }
--- app/src/main/java/com/daniillyubaev/ourawesomeapp/MainViewModel.kt
+++ app/src/main/java/com/daniillyubaev/ourawesomeapp/MainViewModel.kt
@@ -1,9 +1,14 @@
 package com.daniillyubaev.ourawesomeapp
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.daniillyubaev.ourawesomeapp.MainActivity.Companion.LOG_TAG
 import com.squareup.moshi.Moshi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
@@ -12,18 +17,21 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainViewModel : ViewModel() {
 
-    var viewState: ViewState = ViewState.Loading
+    private val _viewState = MutableStateFlow<ViewState>(ViewState.Loading)
+
+    val viewState: Flow<ViewState> get() = _viewState.asStateFlow()
 
     init {
         viewModelScope.launch {
-            viewState = ViewState.Loading
+            _viewState.emit(ViewState.Loading)
             val users = loadUsers()
-            viewState = ViewState.Data(users)
+            _viewState.emit(ViewState.Data(users))
         }
     }
 
     private suspend fun loadUsers(): List<User> {
         return withContext(Dispatchers.IO) {
+            Log.d(LOG_TAG, "loadUsers()")
             Thread.sleep(2999)
             provideApi().getUsers().data
         }
