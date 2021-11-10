f04f4d31bc4a96c0b228ee672b28f171e6e436e8
Daniil Lyubaev <lyubaevda@gmail.com>
2021-11-10 19:26:04 +0300

    Make email confirmation screen

--- app/src/main/java/com/daniillyubaev/ourawesomeapp/ui/emailconfirmation/EmailConfirmationFragment.kt
+++ app/src/main/java/com/daniillyubaev/ourawesomeapp/ui/emailconfirmation/EmailConfirmationFragment.kt
@@ -1,12 +1,25 @@
 package com.daniillyubaev.ourawesomeapp.ui.emailconfirmation
 
+
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.daniillyubaev.ourawesomeapp.ui.base.BaseFragment
 import com.daniillyubaev.ourawesomeapp.R
 import com.daniillyubaev.ourawesomeapp.databinding.FragmentEmailConfirmationBinding
 
 class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmation) {
-    private val viewModel: EmailConfirmationViewModel by viewModels()
+
     private val viewBinding by viewBinding(FragmentEmailConfirmationBinding::bind)
+
+    private val viewModel: EmailConfirmationViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
