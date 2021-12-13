ca0aded0126d33a6d09f3573975b4ee586d069bc
Daniil Lyubaev <lyubaevda@gmail.com>
2021-12-13 16:27:57 +0300

    Add app style

--- app/src/main/java/com/daniillyubaev/ourawesomeapp/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/daniillyubaev/ourawesomeapp/ui/onboarding/OnboardingFragment.kt
@@ -16,6 +16,7 @@ import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
+import dev.chrisbanes.insetter.applyInsetter
 
 class OnboardingFragment : BaseFragment(R.layout.fragment_onboarding) {
 
@@ -36,6 +37,12 @@ class OnboardingFragment : BaseFragment(R.layout.fragment_onboarding) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewBinding.volumeControlButton.applyInsetter {
+            type(statusBars = true) { margin() }
+        }
+        viewBinding.signUpButton.applyInsetter {
+            type(navigationBars = true) { margin() }
+        }
         viewBinding.playerView.player = player
         viewBinding.viewPager.setTextPages()
         viewBinding.viewPager.attachDots(viewBinding.onboaringTextTabLayout)
